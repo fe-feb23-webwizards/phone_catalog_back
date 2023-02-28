b1bf0d9a481a2c561a84e65b093723d8bed28f36
nikachu404 <yasinskaa.veronika@gmail.com>
2023-02-28 02:49:40 +0200

    ESLint, husky, express, typescript

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('Hello, web wizards');
+});
+
+app.listen(PORT, () => {
+  console.log(`API is ready on http://localhost:${PORT}`);
+});
