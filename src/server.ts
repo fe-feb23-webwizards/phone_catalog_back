499d736baf3953131e7c0b08c5833de55e12beac
nikachu404 <yasinskaa.veronika@gmail.com>
2023-03-01 12:23:54 +0200

    lint staged, cors, readme, prettier, added missing husky files

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(cors());
 
 app.use('/', (req, res) => {
   res.send('Hello, web wizards');
 });
 
-app.listen(PORT, ( => {
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`API is ready on http://localhost:${PORT}`);
 });
