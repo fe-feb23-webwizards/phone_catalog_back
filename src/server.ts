3a110b2bb0eebbbf0de0fe56623f31d8aa3020e3
Andrii Petrinko <apetrinko@gmail.com>
2023-03-02 23:28:51 +0200

    removed console log inside forEach method and fixed lit issues

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import fs from 'fs';
 import path from 'path';
-import {Phone} from './types/Phones';
+import { Phone } from './types/Phones';
 
 const app: Application = express();
 
@@ -10,22 +10,26 @@ const phonesDir = path.join(__dirname, './data/phones');
 app.get('/', (req: Request, res: Response) => {
   const phones: Phone[] = [];
 
-  fs.readdir(phonesDir, (err: NodeJS.ErrnoException | null, files: string[]) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send(['Internal Server Error']);
+  fs.readdir(
+    phonesDir,
+    (err: NodeJS.ErrnoException | null, files: string[]) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send(['Internal Server Error']);
+      }
+
+      files.forEach((file) => {
+        const phoneData = fs.readFileSync(`${phonesDir}/${file}`);
+        const phoneJson = JSON.parse(phoneData.toString());
+        phones.push(phoneJson);
+      });
+
+      res.send(phones);
     }
-
-    files.forEach(file => {
-      const phoneData = fs.readFileSync(`${phonesDir}/${file}`);
-      console.log(file)
-      const phoneJson = JSON.parse(phoneData.toString());
-      phones.push(phoneJson);
-    });
-
-    res.send(phones);
-  });
+  );
 });
 
-const port: number = 5000;
-app.listen(port, () => console.log(`Server running on port http://localhost:${port} 🚀`));
+const port = 5000;
+app.listen(port, () =>
+  console.log(`Server running on port http://localhost:${port} 🚀`)
+);
