cbbe908b7bb339d6fc16eea68cbbf84fab82b85c
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-03-06 14:17:58 +0200

    added pagination

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,38 @@
 import express, { Application, Request, Response } from 'express';
 import fs from 'fs';
 import path from 'path';
-import { Phone } from './types/Phones';
 import cors from 'cors';
 
 const app: Application = express();
 
-const phonesDir = path.join(__dirname, '../src/data/phones');
+const productsDir = path.join(__dirname, '../src/data/phones');
 
 app.get('/phones', cors(), (req: Request, res: Response) => {
-  const phones: Phone[] = [];
+  const page = parseInt(req.query.page as string) || 1;
+  const perPage = parseInt(req.query.perPage as string) || 16;
 
   fs.readdir(
-    phonesDir,
+    productsDir,
     (err: NodeJS.ErrnoException | null, files: string[]) => {
       if (err) {
         console.error(err);
         return res.status(500).send(['Internal Server Error']);
       }
 
-      files.forEach((file) => {
-        const phoneData = fs.readFileSync(`${phonesDir}/${file}`);
-        const phoneJson = JSON.parse(phoneData.toString());
-        phones.push(phoneJson);
+      const startIndex = (page - 1) * perPage;
+      const endIndex = startIndex + perPage;
+      const products = files.slice(startIndex, endIndex).map((file) => {
+        const productData = fs.readFileSync(`${productsDir}/${file}`);
+        const productJson = JSON.parse(productData.toString());
+        return productJson;
       });
 
-      res.send(phones);
+      res.send(products);
     }
   );
 });
 
 const port = 5000;
-app.listen(process.env.PORT || port, () =>
-  console.log(`Server running on port http://localhost:${port} 🚀, changes again`)
+app.listen(port, () =>
+  console.log(`Server running on port http://localhost:${port} 🚀`)
 );
