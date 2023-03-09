d1457f7a9474e247a343ec025858924256b6d4ef
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-03-09 13:09:57 +0200

    delete build folder

--- build/types/SortBy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.SortBy = void 0;
-var SortBy;
-(function (SortBy) {
-  SortBy['Newest'] = 'newest';
-  SortBy['Alphabetically'] = 'alphabetically';
-  SortBy['Cheapest'] = 'cheapest';
-})((SortBy = exports.SortBy || (exports.SortBy = {})));
